6d679b2e1c0d70ef4ecff34f5b0350cde2983d34
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-02-14 19:21:28 +0100

    Added CrossOrigin to spring boot

--- backend/src/main/java/com/kxmil/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/com/kxmil/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.kxmil.ecommerce.dao;
 
 import com.kxmil.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>
 {
 }
