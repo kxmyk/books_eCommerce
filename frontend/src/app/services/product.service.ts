54afb125aaf3234f26013a02eacfa32d197fde2e
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-02-22 23:37:07 +0100

    Updated ProductDetails view

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -40,8 +40,10 @@ export class ProductService {
     )
   }
 
-  getProduct(theProductId: number) {
-    
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
   }
 }
 
