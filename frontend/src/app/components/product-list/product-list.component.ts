b3b27cd299e39cc377ba417f74b3b961618e1e98
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-02-23 15:18:01 +0100

    Update ProductListComponent to handle pagination

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -13,7 +13,12 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) {
@@ -46,10 +51,20 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    // if we have different category than previous, reset Page to 1
+    if (this.previousCategoryId !== this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
     // get products for given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+    this.productService.getProductListPaginate(this.thePageNumber - 1, this.thePageSize, this.currentCategoryId).subscribe(
       data => {
-        this.products = data;
+        this.products = data._embedded.products;
+        this.thePageNumber = data.page.number + 1;
+        this.thePageSize = data.page.size;
+        this.theTotalElements = data.page.totalElements;
       }
     )
   }
