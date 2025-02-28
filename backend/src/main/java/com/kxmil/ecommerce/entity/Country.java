e9cc55b54fd4e35299db1312683419a6abdcbc6b
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-02-28 21:08:46 +0100

    Created Country and State classes

---
+++ backend/src/main/java/com/kxmil/ecommerce/entity/Country.java
@@ -0,0 +1,26 @@
+package com.kxmil.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private State state;
+}
