5579bf557b0effe6a418934272027bb50fe4e2dd
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-03-01 11:38:23 +0100

    FrontEnd - Add methods to retrieve countries and states

--- frontend/src/app/services/form.service.ts
+++ frontend/src/app/services/form.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {Country} from '../common/country';
+import {map} from 'rxjs/operators';
+import {State} from '../common/state';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,18 @@ export class FormService {
   private countriesUrl = 'http://localhost:8080/api/countries';
   private statesUrl = 'http://localhost:8080/api/states';
 
-  constructor(httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getCountries(): Observable<Country[]> {
+
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(map(res => res._embedded.countries));
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    const searchUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchUrl).pipe(map(res => res._embedded.states));
   }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
@@ -26,13 +40,25 @@ export class FormService {
   getCreditCardYears(): Observable<number[]> {
     let data: number[] = [];
 
-    const startYear : number = new Date().getFullYear()
-    const endYer : number = startYear + 10;
+    const startYear: number = new Date().getFullYear()
+    const endYer: number = startYear + 10;
 
     for (let theYear = startYear; theYear <= endYer; theYear++) {
       data.push(theYear);
     }
 
     return of(data);
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
   }
 }
