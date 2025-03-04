7cb677e7c84ac31e7f81f2f79224f8c5d64e6ef7
Kamil Lukasiuk <kamilplgt@gmail.com>
2025-03-04 20:01:33 +0100

    Backend - Created classes for saving Order to db

---
+++ backend/src/main/java/com/kxmil/ecommerce/entity/Address.java
@@ -0,0 +1,32 @@
+package com.kxmil.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+}
---
+++ backend/src/main/java/com/kxmil/ecommerce/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.kxmil.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
